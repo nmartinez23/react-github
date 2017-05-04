a953f4daedc75e5a2ca2c4f6be5a90f9a7b2c386
Nick Martinez <23nicholasmartinez@gmail.com>
2017-05-04 16:05:28 -0700

    Update onClick and state for selectedLanguage

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,12 +1,14 @@
 var React = require('react');
 
 class Popular extends React.Component {
+  // initial state of the component
   constructor (props) {
     super(props);
     this.state = {
       selectedLanguage: 'All'
     };
-
+    // bind function sets the context of 'this' to
+    // always reflect the specific component instance
     this.updateLanguage = this.updateLanguage.bind(this);
   }
   updateLanguage(lang) {
@@ -24,7 +26,11 @@ class Popular extends React.Component {
         {languages.map(function (lang) {
           return (
             <li
-              onClick={this.updateLanguage}
+              style={lang === this.state.selectedLanguage ? { color: '#d0021b' }: null}
+              // when any item is clicked the updateLanguage function runs and
+              // updates the selectedLanguage. 'null' is passed in since the
+              // bind context has already been set above.
+              onClick={this.updateLanguage.bind(null, lang)}
               key={lang}>
               {lang}
             </li>
