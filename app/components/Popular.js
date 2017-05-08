e5a6a91b067190c644e67a5da77b434c1c8280d3
Nick Martinez <23nicholasmartinez@gmail.com>
2017-05-08 10:50:20 -0700

    Add API fetchPopulaRepos method

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var PropTypes = require('prop-types');
+var api = require('../utils/api');
 
 // stateless functional component to render some UI
 function SelectLanguage (props) {
@@ -34,18 +35,32 @@ class Popular extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      selectedLanguage: 'All'
+      selectedLanguage: 'All',
+      repos: null
     };
     // bind function sets the context of 'this' to
     // always reflect the specific component instance
     this.updateLanguage = this.updateLanguage.bind(this);
   }
+  componentDidMount () {
+    this.updateLanguage(this.state.selectedLanguage);
+  }
   updateLanguage(lang) {
     this.setState(function () {
       return {
-        selectedLanguage: lang
+        selectedLanguage: lang,
+        repos: null
       }
     });
+
+    api.fetchPopularRepos(lang)
+      .then(function (repos) {
+        this.setState(function () {
+          return {
+            repos: repos
+          }
+        })
+      }.bind(this));
   }
   render() {
 
