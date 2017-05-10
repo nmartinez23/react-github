915e83b9f57dab4334339b0e7cd04c4714dfdb44
Nick Martinez <23nicholasmartinez@gmail.com>
2017-05-10 11:40:28 -0700

    Add handleReset method to the Battle component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -8,7 +8,7 @@ function PlayerPreview (props) {
         <img
           className='avatar'
           src={props.avatar}
-          alt={'Avatar for ' props.username}
+          alt={'Avatar for ' + props.username}
         />
         <h2 className='username'>@{props.username}</h2>
       </div>
@@ -99,12 +99,22 @@ class Battle extends React.Component {
     }
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+
   }
   handleSubmit(id, username) {
     this.setState(function () {
       var newState = {};
       newState[id + 'Name'] = username;
       newState[id + 'Image'] = 'https://github.com/' + username + '.png?size=200';
+      return newState;
+    });
+  }
+  handleReset(id) {
+    this.setState(function () {
+      var newState = {};
+      newState[id + 'Name'] = '';
+      newState[id + 'Image'] = null;
       return newState;
     });
   }
