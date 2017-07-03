a993d472b47528618fb3d56d3b7204bbf3dac1d3
Nick Martinez <23nicholasmartinez@gmail.com>
2017-07-03 08:26:21 -0700

    Add Results component

--- app/components/App.js
+++ app/components/App.js
@@ -1,5 +1,4 @@
 var React = require('react');
-var Popular = require('./Popular');
 var ReactRouter = require('react-router-dom');
 var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
@@ -7,6 +6,8 @@ var Switch = ReactRouter.Switch;
 var Nav = require('./Nav');
 var Home = require('./Home');
 var Battle = require('./Battle');
+var Popular = require('./Popular');
+var Results = require('./Results');
 
 class App extends React.Component {
   render() {
@@ -16,7 +17,8 @@ class App extends React.Component {
           <Nav />
           <Switch>
             <Route exact path='/' component={Home} />
-            <Route path='/battle' component={Battle} />
+            <Route exact path='/battle' component={Battle} />
+            <Route path='/battle/results' component={Results} />
             <Route path='/popular' component={Popular} />
             <Route render={function () {
               return <p>Not Found</p>
